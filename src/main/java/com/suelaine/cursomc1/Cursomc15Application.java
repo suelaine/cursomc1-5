09a4a62ac9c11ae6e8f392c8fafe74a9edff3fa1
Suelaine Andrade <suelaine.andrade@gmail.com>
2018-07-13 18:04:02 -0300

    Versão Inicial Projeto Criado

---
+++ src/main/java/com/suelaine/cursomc1/Cursomc15Application.java
@@ -0,0 +1,12 @@
+package com.suelaine.cursomc1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Cursomc15Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Cursomc15Application.class, args);
+	}
+}
