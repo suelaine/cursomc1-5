baffd1307e1ffc6841163e3b55490534c1de419a
Suelaine Andrade <suelaine.andrade@gmail.com>
2018-07-13 18:36:31 -0300

    Testando a Primeira Classe de Domínio

--- src/main/java/com/suelaine/cursomc1/resources/CategoriaResource.java
+++ src/main/java/com/suelaine/cursomc1/resources/CategoriaResource.java
@@ -1,17 +1,29 @@
 package com.suelaine.cursomc1.resources;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.suelaine.cursomc1.Categoria;
 
 @RestController
 @RequestMapping(value = "categorias")
 public class CategoriaResource {
 	
 	@RequestMapping(method = RequestMethod.GET)
-	public String Listar() {
+	public List<Categoria> Listar() {
 		
-		return "Rest OK";
+		Categoria cat1 = new Categoria(1,"Informática");
+		Categoria cat2 = new Categoria(2,"Escritório");
+		
+		List<Categoria> lista = new ArrayList<>();
+		lista.add(cat1);
+		lista.add(cat2);
+		
+		return lista;
 	}
 
 }
